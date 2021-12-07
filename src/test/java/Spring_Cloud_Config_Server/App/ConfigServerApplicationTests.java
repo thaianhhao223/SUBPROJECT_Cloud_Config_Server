5f62c1b94cba0ef836f9a282eb2b90babd6fc7b0
thaianhhao223 <thaianhhao322@gmail.com>
2021-12-07 23:30:40 +0700

    Cloud config server

---
+++ src/test/java/Spring_Cloud_Config_Server/App/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_Cloud_Config_Server.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
